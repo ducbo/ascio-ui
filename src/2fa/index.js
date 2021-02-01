958b4bd7ccd363d0f106d9505a5feff8ae12bb84
rendermani <ml@webrender.de>
2021-02-01 23:07:16 +0100

    ResetQR and ResetPassword

--- src/2fa/index.js
+++ src/2fa/index.js
@@ -3,3 +3,5 @@ export * from './Code';
 export * from './Activate';
 export * from './Force';
 export * from './LoginButton';
+export * from './ResetQrButton';
+export * from './ResetPasswordButton';
