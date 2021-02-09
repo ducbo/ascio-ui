dd9902745271f1c3eb139ccc68e324ad8bcc30ae
rendermani <ml@webrender.de>
2021-02-10 00:19:04 +0100

    add dot information text

--- src/Record/Record.js
+++ src/Record/Record.js
@@ -3,81 +3,35 @@ import { connect } from 'react-redux';
 import { recordActions } from '../_actions';
 import { recordToApi } from '../_helpers';
 import { Combobox } from 'react-widgets';
-import {TTL} from './TTL'
-import {RedirectionType} from './RedirectionType'
+import { TTL } from './TTL';
+import { RedirectionType } from './RedirectionType';
+import fields from './fields';
 
-
-function renderInfoText(zone,Source,Target,text,_type) {
-		const record = {Source, Target, _type}		
-		if((Source && Target) || _type === "SOA") {
-			const newRecord = recordToApi(zone,record,"Ascio")
-            return <span><b>{newRecord.Source}</b> {text} <b>{newRecord.Target}</b></span>
-        } else {
-            return "Please enter Source and Target"
-        }
-    
-    
-}
-
-const fields = {
-	A: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'IPv4 Address',
-		text: "will point to"
-	},
-	AAAA: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'IPv6 Address',
-		text: "will point to"
-	},
-	CNAME: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'Alias',
-		text: "will be an alias of"
-	},
-	MX: {
-		list: [ 'Source', 'Target', 'TTL', 'Priority' ],
-		targetDescription: 'Mail server',
-		text: "will handle mail exchange for"
-	},
-	TXT: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'Content',
-		text: "will include"
-	},
-	NS: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'Name Server',
-		text: "will be managed by"
-	},
-	SRV: {
-		list: [ 'Source', 'Target', 'TTL', 'Priority', 'Weight', 'Port' ],
-		targetDescription: 'Target',
-		text: "will be serviced by"
-	},
-	SOA: {
-		list: [ 'Source', 'TTL', 'Expire', 'HostmasterEmail', 'PrimaryNameServer', 'Refresh', 'Retry' ],
-		targetDescription: 'Value',
-		text: "will be defined by",
-		type: "SOA"
-	},
-	MailForward: {
-		list: [ 'Source', 'Target', 'TTL' ],
-		targetDescription: 'Email Address',
-		text: "will forward to"
-	},
-	WebForward: {
-		list: [ 'Source', 'Target', 'TTL','RedirectionType' ],
-		targetDescription: 'Website URL',
-		text: "will redirect to"
+function renderInfoText(zone, Source, Target, text, _type) {
+	const record = { Source, Target, _type };
+	if ((Source && Target) || _type === 'SOA') {
+		const newRecord = recordToApi(zone, record, 'Ascio');
+		const dot = fields[_type].dot
+		let dotText = ""
+		if(dot && (Target.indexOf(".") > -1)) {
+			dotText = <span>Please add a  <b>dot</b> at the end for a targets outside of  {zone}</span>
+		}
+		return (
+			<span>
+				<b>{newRecord.Source}</b> {text} <b>{newRecord.Target}</b><br></br>
+				{dotText}
+			</span>
+		);
+	} else {
+		return 'Please enter Source and Target';
 	}
-};
+}
 
 class Record extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: props.data || {TTL: 3600, RedirectionType: "Permanent"},
+			data: props.data || { TTL: 3600, RedirectionType: 'Permanent' },
 			zone: props.zone,
 			action: props.action
 		};
@@ -132,11 +86,24 @@ class Record extends Component {
 	renderField(field) {
 		const typeFields = fields[this.state.data._type];
 		const placeHolder = field == 'Target' ? typeFields.targetDescription : field;
-		let input = ""
+		let input = '';
 		switch (field) {
-			case "RedirectionType" : input = <RedirectionType onChange={this.onChange} value={this.state.data[field]}></RedirectionType>; break;
-			default: input = <input	type="text"	name={field} className="form-control" id={'input' + field} value={this.state.data[field]} onChange={this.onChange} placeholder={placeHolder}/>
-		}	
+			case 'RedirectionType':
+				input = <RedirectionType onChange={this.onChange} value={this.state.data[field]} />;
+				break;
+			default:
+				input = (
+					<input
+						type="text"
+						name={field}
+						className="form-control"
+						id={'input' + field}
+						value={this.state.data[field]}
+						onChange={this.onChange}
+						placeholder={placeHolder}
+					/>
+				);
+		}
 		return (
 			<div className="form-group row">
 				<label htmlFor={'input' + field} className="col-sm-2 col-form-label">
@@ -151,47 +118,49 @@ class Record extends Component {
 		const self = this;
 		const typeFields = fields[this.state.data._type];
 		typeFields.list.forEach((field) => {
-            if(field=="TTL") return;
+			if (field == 'TTL') return;
 			html.push(self.renderField(field));
 		});
 		return html;
 	}
-	renderCreateForm(ttl,use) {
-		let type = "";
-		if(use.type) {
-			type = <div className="form-group row">
-			<label htmlFor="typeSelect" className="col-sm-2 col-form-label">
-				Type
-			</label>
-			<div className="col-sm-10">
-				<select id="typeSelect" className="form-control" onChange={this.setNewType}>
-					<option>A</option>
-					<option>AAAA</option>
-					<option>CNAME</option>
-					<option>TXT</option>
-					<option>SRV</option>
-					<option>MX</option>
-					<option>NS</option>
-					<option>WebForward</option>
-					<option>MailForward</option>
-				</select>
-			</div>
-		</div>
+	renderCreateForm(ttl, use) {
+		let type = '';
+		if (use.type) {
+			type = (
+				<div className="form-group row">
+					<label htmlFor="typeSelect" className="col-sm-2 col-form-label">
+						Type
+					</label>
+					<div className="col-sm-10">
+						<select id="typeSelect" className="form-control" onChange={this.setNewType}>
+							<option>A</option>
+							<option>AAAA</option>
+							<option>CNAME</option>
+							<option>TXT</option>
+							<option>SRV</option>
+							<option>MX</option>
+							<option>NS</option>
+							<option>WebForward</option>
+							<option>MailForward</option>
+						</select>
+					</div>
+				</div>
+			);
 		}
 		return (
 			<div>
 				<div className="row">
-				<div className="col-md-2">
-					 {type}
-					 <TTL onChange={this.onChange} value={ttl}></TTL>		
+					<div className="col-md-2">
+						{type}
+						<TTL onChange={this.onChange} value={ttl} />
+					</div>
+					<div className="col-md-8">{this.renderFields()}</div>
 				</div>
-				<div className="col-md-8">{this.renderFields()}</div>
-				</div>				
 			</div>
 		);
 	}
 	renderTTL(value) {
-		return <TTL onChange={this.onChange} value={value}></TTL>		
+		return <TTL onChange={this.onChange} value={value} />;
 	}
 	render() {
 		let html = [];
@@ -212,7 +181,7 @@ class Record extends Component {
 			if (success) {
 				message = (
 					<div className="alert alert-success   mt-2 col-md-12" role="alert">
-						{this.props.action == 'create' ? 'Create record successful' : 'Update record successful'} 
+						{this.props.action == 'create' ? 'Create record successful' : 'Update record successful'}
 					</div>
 				);
 			}
@@ -223,13 +192,13 @@ class Record extends Component {
 		});
 		let form = '';
 		if (this.state.action == 'create') {
-			form = this.renderCreateForm(3600,{type:true});
+			form = this.renderCreateForm(3600, { type: true });
 		} else {
-			form = this.renderCreateForm(this.state.data.TTL,{});
+			form = this.renderCreateForm(this.state.data.TTL, {});
 		}
 
 		return (
-			<div key ={this.state.data.Id}>
+			<div key={this.state.data.Id}>
 				{form}
 				<div className="row">
 					<div className="col-md-2">
@@ -243,21 +212,21 @@ class Record extends Component {
 							{this.state.action} record
 						</button>
 					</div>
-					<div className="col-md-8"> <div className="alert alert-secondary" role="alert">
-                    {renderInfoText(
-						self.props.zone,
-						self.state.data.Source,
-						self.state.data.Target || self.state.data.PrimaryNameServer,
-						typeFields.text,
-						self.state.data._type
-						)}
-					</div></div>
+					<div className="col-md-8">
+						{' '}
+						<div className="alert alert-secondary" role="alert">
+							{renderInfoText(
+								self.props.zone,
+								self.state.data.Source,
+								self.state.data.Target || self.state.data.PrimaryNameServer,
+								typeFields.text,
+								self.state.data._type
+							)}
+						</div>
+					</div>
 				</div>
 				<div className="row">
-					<div className="col-md-10">
-					{message}
-					</div>
-				
+					<div className="col-md-10">{message}</div>
 				</div>
 			</div>
 		);
@@ -274,4 +243,4 @@ function mapState(state) {
 	return { user, users, records };
 }
 const connectedRecord = connect(mapState, actionCreators)(Record);
-export {connectedRecord as Record};
+export { connectedRecord as Record };
