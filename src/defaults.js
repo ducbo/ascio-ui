de72ab5037f6b0424fbc81eddfc0953c7f78e2b3
rendermani <ml@webrender.de>
2021-04-16 02:27:31 +0200

    add default user from localstorage

--- src/defaults.js
+++ src/defaults.js
@@ -1,6 +1,11 @@
 const getFilters = function(name,defaultSortField) {
 	return function (user)  {	
 		const storageFilters = localStorage.getItem('default'+name+'Filters_' + user);
+		const impersonatedJson = localStorage.getItem('customer_tree_'+user+'_selected');
+		let impersonated = null
+		if(impersonatedJson) {
+			impersonated = JSON.parse(impersonatedJson)
+		} 
 		if (storageFilters) {
 			const result =  JSON.parse(storageFilters);
 			if(!result.page || result.page < 1) result.page=1;
@@ -8,6 +13,7 @@ const getFilters = function(name,defaultSortField) {
 			if(!result.sortField ) result.sortField=defaultSortField;
 			if(!result.sortOrder) result.sortOrder="asc";
 			if(!result.filter) result.filter="*";
+			if(impersonated) result.users = impersonated
 			return result
 		} else {
 			return {
@@ -16,7 +22,8 @@ const getFilters = function(name,defaultSortField) {
 				filter: '*',
 				sortField: defaultSortField,
 				sortOrder: 'desc',
-				type: name.toLowerCase()
+				type: name.toLowerCase(),
+				users : impersonated
 			};
 		}
 	}
