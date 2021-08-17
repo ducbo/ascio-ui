092dda23e67ceb8a9cd21738790e542047b91495
rendermani <ml@webrender.de>
2021-08-17 22:39:22 +0200

    delete console.log

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -15,7 +15,6 @@ class HomePage extends React.Component {
 
     render() {
         const { user, users } = this.props;
-        console.log(user)
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h1>Hi {user.user.username}!</h1>
