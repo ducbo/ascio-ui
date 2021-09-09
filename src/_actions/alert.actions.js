3d8082fcfc78145d677567518fe31f62a5b30eea
rendermani <ml@webrender.de>
2021-09-09 23:44:16 +0200

    add message and progress

--- src/_actions/alert.actions.js
+++ src/_actions/alert.actions.js
@@ -2,18 +2,32 @@ import { alertConstants } from '../_constants';
 
 export const alertActions = {
     success,
+    message,
     error,
-    clear
+    clear,
+    progress
 };
+
+
+function message(result) {
+    if(result.success) {
+        return success(result.success)
+    } else if (result.error) {
+        return error(result.error)
+    } else {
+        return progress(result.progress)
+    }  
+}
 
 function success(message) {
     return { type: alertConstants.SUCCESS, message };
 }
-
 function error(message) {
     return { type: alertConstants.ERROR, message };
 }
-
+function progress(message) {
+    return { type: alertConstants.PROGRESS, message };
+}
 function clear() {
     return { type: alertConstants.CLEAR };
 }
