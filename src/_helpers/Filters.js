f27cecd4b6c82f2099daa39cecd86f279114e394
rendermani <ml@webrender.de>
2021-10-12 22:02:51 +0200

    add new Filters

---
+++ src/_helpers/Filters.js
@@ -0,0 +1,94 @@
+class Range {
+    setFrom(from, include) {
+        this.from = from
+        this.includeFrom = include === undefined ? true : false
+        return this
+    }
+    setTo(to,include) {
+        this.to = to
+        this.includeTo = include === undefined ? true : false
+        return this
+    }
+    isValid() {
+        return this.to || this.from
+    }
+}
+class FilterElement {
+    constructor(name) {
+        this.name = name 
+        this.type = this.constructor.name
+    }
+    setValue(value) {
+        this.value = value
+        return this
+    }
+    setType(type) {
+        this.type = type
+        return this
+    }
+    setName(name) {
+        this.name = name;
+        return this
+    }
+    isValid() {
+        return this.name && this.value
+    }
+    setTableFilter(name, filter) {
+        return  this.setValue(filter.filterVal)           
+    }
+}
+class RangeFilterElement extends FilterElement {
+    constructor (name) {
+        super(name)
+        this.range = new Range()
+        
+    }
+    isValid() {
+        return this.name && this.range.isValid() && this.operator
+    }
+}
+class DateFilterElement extends RangeFilterElement {
+    setTableFilter(filter) {
+        const value = filter.filterVal
+        this.operator = value.comparator
+        const date = value.date
+        if(!date) return
+        const dateString = date.getFullYear() + "/" +( date.getMonth() +1) + "/" + date.getDate()
+        const currentDay = new Date(dateString)
+        const nextDay = new Date(dateString)
+        nextDay.setDate(nextDay.getDate() +1)
+        switch(this.operator) {
+            case ">" : this.range.setFrom(nextDay.getTime()); break
+            case ">=" : this.range.setFrom(currentDay.getTime())  ; break
+            case "=" :  this.range.setFrom(currentDay.getTime()).setTo(nextDay.getTime()); break
+            case "<" : this.range.setTo(currentDay.getTime(),false); break
+            case "<=" : this.range.setTo(nextDay.getTime()); break
+        }
+        return this
+    }
+}
+export class Filters {
+    constructor(tableFilters) {
+        this.items = {}
+        if(tableFilters) this.addTableFilters(tableFilters)
+    }
+    addTableFilters(tableFilters) {
+        for(const filterName in tableFilters) {
+            const filter = tableFilters[filterName]
+            this.type = filter.filterType
+            let filterElement
+            switch(filter.filterType) {
+                case "DATE":  filterElement = new DateFilterElement(filterName); break
+                default:  filterElement = new FilterElement(filterName)       
+            }
+            filterElement.setTableFilter(filter)
+            if(filterElement.isValid()) {
+                this.items[filterName] = filterElement
+            }            
+        }
+    }
+    get() {
+        return this.items
+    }
+
+}
--- src/_helpers/index.js
+++ src/_helpers/index.js
@@ -3,3 +3,4 @@ export * from './store';
 export * from './auth-header';
 export * from './zoneformat';
 export * from './pw-strength';
+export * from './Filters';
