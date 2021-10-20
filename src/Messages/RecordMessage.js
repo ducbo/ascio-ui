86fcea107535b6bc4a404b6eac566435ce4a3df8
rendermani <ml@webrender.de>
2021-10-20 23:30:42 +0200

    rename wrong name

--- src/Messages/RecordMessage.js
+++ src/Messages/RecordMessage.js
@@ -1,6 +1,6 @@
 const {Message} = require(".")
 
-module.exports =  class UserMessage extends Message {
+module.exports =  class RecordMessage extends Message {
     getTitle() {
         return "["+this.loginUser.username+"] " + this.action.charAt(0).toUpperCase() + this.action.slice(1) + " "+ this.dataClass + ": "+ this.objectName
     }
