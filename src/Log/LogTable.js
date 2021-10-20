e1765163eab5b0c90ecb8a2651d4aa1889488c34
rendermani <ml@webrender.de>
2021-10-20 23:30:20 +0200

    improve Filters

--- src/Log/LogTable.js
+++ src/Log/LogTable.js
@@ -2,13 +2,14 @@ import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import filterFactory, { textFilter, dateFilter, multiSelectFilter } from 'react-bootstrap-table2-filter';
-import { history,Filters } from '../_helpers';
 import { connect } from 'react-redux';
 import { logActions } from '../_actions';
 import {defaultLogFilters} from '../defaults.js'
 import {Modal, Button} from 'react-bootstrap'
 import { FaEye } from 'react-icons/fa';
-import {LogLevel} from './LogLevel'
+import {LogLevel,Entry} from '.'
+import { Filters, FilterElement } from '../_helpers';
+
 
 function dateFormatter (date, row) {    
     let month = date.getMonth() + 1;
@@ -22,17 +23,7 @@ function dateFormatter (date, row) {
     min = (min < 10 ? "0" : "") + min;
     return day + "." + month + "." + date.getFullYear() + " - " +hour + ":" + min
 }
-const selectOptions = {
-        "completed" : "completed",
-        "failed" : "failed",
-        "waiting" : "waiting",
-        "security" : "security", 
-        "error" : "error",        
-        "warn" : "warn",
-        "info" : "info",
-        "verbose" : "verbose",
-        "debug" : "debug"
-};
+
 const cellFormater  = (cell, row, rowIndex, colIndex,width) => {
   let css = {
     padding: 0,
@@ -42,11 +33,6 @@ const cellFormater  = (cell, row, rowIndex, colIndex,width) => {
 
     return css
 }
-const editButton = (cellContent) => {
-  const target = cellContent.currentTarget.dataset.row
-  history.push("/zone/" + target)
-}
-
 class LogTable extends React.Component {
   constructor(props) {
     super(props);
@@ -62,14 +48,14 @@ class LogTable extends React.Component {
       lastUsers : null,
       options : [],
       refresh : false
-     // users : props.users
-      }
+    }
 
     this.rootDescendants = this.props.rootDescendants
 
   }
   getColumns = () => {
-    const columns1 = [{
+    const logLevel = new LogLevel()
+    this.columns = [{
       dataField: 'CreatedDate',
       text: 'Date',
       formatter: dateFormatter, 
@@ -78,7 +64,7 @@ class LogTable extends React.Component {
       sort: true,
       headerStyle: {width:"180px"}
     }, {
-      dataField: 'loginUser__username',
+      dataField: 'loginUser.username',
       text: 'Login',
       sort: true,
       style : cellFormater,
@@ -104,7 +90,7 @@ class LogTable extends React.Component {
         },
         style: cellFormater,
         filter: multiSelectFilter({
-            options: selectOptions
+            options: logLevel.formatForTable()
           }),
         headerStyle: {width:"135px"}    
       },
@@ -120,13 +106,9 @@ class LogTable extends React.Component {
         text: 'View',
         style : cellFormater,
         headerStyle: {width:"70px"},
-        formatter: (cellContent, row) => { return <><button title="Edit Zone" className="btn edit-button"  data-row={row.ZoneName} onClick={editButton}><FaEye  size="20px"></FaEye></button> </>}
+        formatter: (cellContent, row) => { return <FaEye style={{color:'grey'}} size="20px"></FaEye>}
       }
 ];
-    const columns3 = [];
-
-    const user = JSON.parse(localStorage.getItem('user'))
-    this.columns = columns1.concat(columns3)
     return this.columns
   }
   componentDidMount() {
@@ -139,15 +121,17 @@ class LogTable extends React.Component {
     // this.setState({options: this.props.rootDescendants})
     const searchParameters = defaultLogFilters(this.user.username)
     searchParameters.users = this.getImpersonated()
+    searchParameters.filter = this.getFilters(searchParameters.filter).items
     if(searchParameters.users !== this.state.lastUsers) {
       this.props.filter(searchParameters,this.props.log)
       this.setState({lastUsers:searchParameters.users})
     }
   }
-
+  getFilters(filters) {
+    return new Filters(filters)
+  }
   async handleTableChange  (type, { page, sizePerPage, filters, sortField, sortOrder, cellEdit }) {
-    const self = this
-    const filter = new Filters(filters)
+    const filter = this.getFilters(filters)
     const users = this.getImpersonated();
     sortField = sortField || defaultLogFilters(this.user.username).sortField
     sortOrder = sortOrder  || defaultLogFilters(this.user.username).sortOrder    
@@ -184,6 +168,10 @@ class LogTable extends React.Component {
       filter={ filterFactory() }
       pagination={ paginationFactory({  page, sizePerPage, totalSize }) }
       onTableChange={ this.handleTableChange  }  
+      expandRow={{
+        showExpandColumn: true,
+        renderer: row => (<Entry data={{...row}} key = {row.Id}></Entry>)
+      }}
     />  
   }
 }
@@ -193,7 +181,7 @@ const actionCreators = {
 function mapState(state) {
   const { user } = state.authentication;
   const { data, refresh,success, progress } = state.log;
-  const { rootDescendants, selectableUsers, descendants, impersonate } = state.usertree;
+  const { impersonate } = state.usertree;
   return { user, data,  impersonate, refresh, zoneMessage : {success,progress} };
 }
 const connectedLogTable = connect(mapState, actionCreators)(LogTable)
