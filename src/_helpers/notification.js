121a69b541b5ef94d5601f3833565c9a0006f13b
rendermani <ml@webrender.de>
2021-10-25 18:10:51 +0200

    custom notifications without double display

---
+++ src/_helpers/notification.js
@@ -0,0 +1,31 @@
+import { store } from 'react-notifications-component';
+
+const maxSize = 100
+const lastMessages = []
+
+function notify(title,message,key) {
+    if(lastMessages.includes(key)) {
+        return
+    } else {
+        store.addNotification({
+            title: title,
+            id: key,
+            message: message,
+            type: "success",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],						
+            dismiss: {
+              duration: 5000,
+              pauseOnHover: true,
+              onScreen: true
+            }
+        });
+        lastMessages.push(key)
+        if(lastMessages.length >= maxSize) {
+            lastMessages.shift()
+        }
+    }
+}
+export {notify as notify}
